df641a451f4573c76bf966f724cab0277623d2cd
yokozawa0701 <d.yoko.0701@gmail.com>
2021-11-08 23:36:35 +0900

    境界値テストの追加

--- GildedRose/gilded_rose_spec.rb
+++ GildedRose/gilded_rose_spec.rb
@@ -22,7 +22,7 @@ describe GildedRose do
         expect(items[0].sell_in).to eq 29
       end
 
-      context 'Item#qualityが0の時' do
+      context 'Item#qualityが0の場合' do
         let(:quality) { 0 }
         it 'Item#qualityがマイナスにならないこと' do
           items = [Item.new(name, sell_in, quality)]
@@ -31,7 +31,7 @@ describe GildedRose do
         end
       end
 
-      context 'Item#qualityが50の時' do
+      context 'Item#qualityが50の場合' do
         let(:quality) { 50 }
         it 'Item#qualityが1小さくなること' do
           items = [Item.new(name, sell_in, quality)]
@@ -46,7 +46,7 @@ describe GildedRose do
         end
       end
 
-      it 'Item#sell_inが0の時、Item#qualityが2小さくなること' do
+      it 'Item#sell_inが0の場合、Item#qualityが2小さくなること' do
         items = [Item.new(name, 0, quality)]
         GildedRose.new(items).update_quality
         expect(items[0].quality).to eq 18
@@ -67,6 +67,15 @@ describe GildedRose do
         GildedRose.new(items).update_quality
         expect(items[0].quality).to eq 50
       end
+
+      context 'Item#qualityが0の場合' do
+        let(:quality) { 0 }
+        it 'Item#qualityが1大きくなること' do
+          items = [Item.new(name, sell_in, quality)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 1
+        end
+      end
     end
 
     context 'Item#nameが Sulfuras, Hand of Ragnaros の場合' do
@@ -82,6 +91,68 @@ describe GildedRose do
         items = [Item.new(name, sell_in, quality)]
         GildedRose.new(items).update_quality
         expect(items[0].quality).to eq quality
+      end
+    end
+
+    context 'Item#nameが Backstage passes to a TAFKAL80ETC concert の場合' do
+      let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
+
+      it 'Item#qualityが1大きくなること' do
+        items = [Item.new(name, sell_in, quality)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 21
+      end
+
+      context 'Item#qualityが0の場合' do
+        let(:quality) { 0 }
+        it 'Item#qualityが1大きくなること' do
+          items = [Item.new(name, sell_in, quality)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 1
+        end
+      end
+
+      context 'Item#sell_inが' do
+        context '10日以内の場合' do
+          let(:sell_in) { 10 }
+          it 'Item#qualityが2大きくなること' do
+            items = [Item.new(name, sell_in, quality)]
+            GildedRose.new(items).update_quality
+            expect(items[0].quality).to eq 22
+          end
+          context 'Item#qualityが0の場合' do
+            let(:quality) { 0 }
+            it 'Item#qualityが2大きくなること' do
+              items = [Item.new(name, sell_in, quality)]
+              GildedRose.new(items).update_quality
+              expect(items[0].quality).to eq 2
+            end
+          end
+        end
+        context '5日以内の場合' do
+          let(:sell_in) { 5 }
+          it 'Item#qualityが3大きくなること' do
+            items = [Item.new(name, sell_in, quality)]
+            GildedRose.new(items).update_quality
+            expect(items[0].quality).to eq 23
+          end
+          context 'Item#qualityが0の場合' do
+            let(:quality) { 0 }
+            it 'Item#qualityが3大きくなること' do
+              items = [Item.new(name, sell_in, quality)]
+              GildedRose.new(items).update_quality
+              expect(items[0].quality).to eq 3
+            end
+          end
+        end
+      end
+
+      context 'Item#sell_inが0の場合' do
+        it 'Item#qualityが0になること' do
+          items = [Item.new(name, 0, quality)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 0
+        end
       end
     end
   end
