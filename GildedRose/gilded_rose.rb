f86fb133cdc011bb935d2a44de16ac677dd46c0b
yokozawa0701 <d.yoko.0701@gmail.com>
2021-11-08 22:15:14 +0900

    sell_inに関わらない部分を関数に切り出した。

--- GildedRose/gilded_rose.rb
+++ GildedRose/gilded_rose.rb
@@ -13,24 +13,30 @@ class GildedRose
 
   private
 
-  def update_item(item)
+  def calc_quality_by_normal(item)
+    result = 0
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality.positive? && item.name != 'Sulfuras, Hand of Ragnaros'
-        item.quality = item.quality - 1
+        result -= 1
       end
     else
       if item.quality < 50
-        item.quality = item.quality + 1
+        result += 1
         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
           if item.sell_in < 11 && item.quality < 50
-            item.quality = item.quality + 1
+            result += 1
           end
           if item.sell_in < 6 && item.quality < 50
-            item.quality = item.quality + 1
+            result += 1
           end
         end
       end
     end
+    result
+  end
+
+  def update_item(item)
+    item.quality += calc_quality_by_normal(item)
     if item.name != 'Sulfuras, Hand of Ragnaros'
       item.sell_in = item.sell_in - 1
     end
