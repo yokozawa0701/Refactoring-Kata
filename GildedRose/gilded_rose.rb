575c8b651292e2d8e930b21ea7eb08d65679452b
yokozawa0701 <d.yoko.0701@gmail.com>
2021-11-09 23:57:03 +0900

    後置ifを使ってネストを減らした。

--- GildedRose/gilded_rose.rb
+++ GildedRose/gilded_rose.rb
@@ -34,20 +34,16 @@ class GildedRose
 
     item.quality += calc_quality_by_normal(item)
     item.sell_in = item.sell_in - 1
-    if item.sell_in.negative?
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality.positive?
-            item.quality = item.quality - 1
-          end
-        else
-          item.quality -= item.quality
-        end
+    return unless item.sell_in.negative?
+
+    if item.name != 'Aged Brie'
+      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        item.quality = item.quality - 1 if item.quality.positive?
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
+        item.quality -= item.quality
       end
+    else
+      item.quality = item.quality + 1 if item.quality < 50
     end
   end
 end
