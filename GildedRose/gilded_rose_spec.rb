dfa8df50b2002df5fd85008da53ade07cb4ae790
yokozawa0701 <d.yoko.0701@gmail.com>
2021-11-13 14:20:51 +0900

    qualityが49の場合のテスト追加

--- GildedRose/gilded_rose_spec.rb
+++ GildedRose/gilded_rose_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require File.join(File.dirname(__FILE__), 'gilded_rose_org')
 
 describe GildedRose do
   describe '#update_quality' do
